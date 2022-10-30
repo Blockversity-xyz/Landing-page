5a2dd58b593af9fd72913ab989257643e8a1e7d5
reyane remache <reyane.a.remache@gmail.com>
2022-10-30 20:07:01 +0100

    fixed text placeent and form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+const btn_form = document.getElementById('form-toggle')
+const div = document.getElementById('form')
+div.style.display = "none"
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open')
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
 })
+
+btn_form.addEventListener("click", () => {
+
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+});
